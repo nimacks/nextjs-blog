4fca9620738debcfc50f6b07e2a38ff3ce7ae8d7
Jared Franzone <jffranzo@amazon.com>
2021-02-01 10:50:26 -0800

    blank

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,7 @@
-import { Amplify, API, Auth, withSSRContext } from "aws-amplify";
-import awsExports from "../../src/aws-exports";
-// import { createPost } from "../src/graphql/mutations";
-import { listTodos } from "../../src/graphql/queries";
-Amplify.configure({ ...awsExports, ssr: true });
-
-export default async (req, res) => {
+export default (req, res) => {
     console.log("the request...");
-    console.log("this is all ran on the server (in a lambdaaaaaaaaaaa)");
+    console.log("this is all ran on the server (in a lambda)");
 
     console.log(req);
-
-
-    const SSR = withSSRContext({ req });
-    const response = await SSR.API.graphql({ query: listTodos });
-  
-    res.status(200).json({ text: 'Hello' + JSON.stringify(response) });
+    res.status(200).json({ text: 'Hello' });
 }
