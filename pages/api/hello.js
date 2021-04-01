f5ba88bf80b3dc6a4413bdf29508311559ed77d6
Jared Franzone <jffranzo@amazon.com>
2021-04-01 13:57:51 -0700

    got

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,22 +1,5 @@
-export default async (req, res) => {
+export default (req, res) => {
     console.log("the request...");
     console.log("this is all ran on the server (in a lambda)");
-
-    const got = require('got');
-    try {
-        console.log("hit: https://6obli1j4bb.execute-api.us-west-2.amazonaws.com/Prod/hello");
-
-        const response = await got('https://6obli1j4bb.execute-api.us-west-2.amazonaws.com/Prod/hello', { json: true });
-        console.log("got restp" + JSON.stringify(response));
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world: ' + JSON.stringify(response),
-                // location: ret.data.trim()
-            })
-        }
-      } catch (error) {
-        console.log(error.response.body);
-        return error;
-      }
+    res.status(200).json({ text: 'Hello 02.21.21 v2' });
 }
