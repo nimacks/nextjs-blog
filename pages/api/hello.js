d2e0482ce6382e739df58a4ef181ea9da4fecea4
Jared Franzone <jffranzo@amazon.com>
2021-04-01 13:18:53 -0700

    update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 export default (req, res) => {
     console.log("the request...");
     console.log("this is all ran on the server (in a lambda)");
-    res.status(200).json({ text: 'Hello 02.21.21 v2' });
+    const request = require('request');
+
+    request('https://6obli1j4bb.execute-api.us-west-2.amazonaws.com/Prod/hello', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log("body: " + JSON.stringify(body));
+        res.status(200).json({ text: JSON.stringify(body) });
+    });
 }
